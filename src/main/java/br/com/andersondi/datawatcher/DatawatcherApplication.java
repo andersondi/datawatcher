2f5028c4ad5b09545a340e082c9a922654c357cc
Anderson Dias <oliveiradiasa@gmail.com>
2020-07-26 23:26:04 -0300

    Initial commit

---
+++ src/main/java/br/com/andersondi/datawatcher/DatawatcherApplication.java
@@ -0,0 +1,13 @@
+package br.com.andersondi.datawatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatawatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatawatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/andersondi/datawatcher/DatawatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andersondi.datawatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatawatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
