03b3118f138682b331007feb7161439d7598d77b
Anderson Dias <oliveiradiasa@gmail.com>
2020-07-29 00:27:51 -0300

    feat(monitor): add directory observer

---
+++ src/main/java/br/com/andersondi/datawatcher/Service/DirectoryMonitor.java
@@ -0,0 +1,61 @@
+package br.com.andersondi.datawatcher.Service;
+
+import br.com.andersondi.datawatcher.GetPropertyValues;
+import java.io.IOException;
+import java.nio.file.*;
+import static java.lang.System.getProperty;
+import static java.nio.file.StandardWatchEventKinds.*;
+
+public class DirectoryMonitor {
+    GetPropertyValues properties = new GetPropertyValues();
+    private final String HOMEPATH = getProperty( "user.home" );
+    private Path dir = Paths.get( HOMEPATH + properties.getPropValues( "inputPath" ) );
+    private final WatchService watcher;
+    DataReader dataReader = new DataReader();
+
+    public DirectoryMonitor() throws IOException {
+        this.watcher = FileSystems.getDefault().newWatchService();
+        this.dir.register( watcher, ENTRY_CREATE, ENTRY_MODIFY );
+        dataReader.processFiles();
+    }
+
+    public void processEvents() {
+        for ( ; ; ) {
+            WatchKey key;
+            try {
+                key = watcher.take();
+            } catch ( InterruptedException x ) {
+                return;
+            }
+
+            for ( WatchEvent< ? > event : key.pollEvents() ) {
+                WatchEvent.Kind kind = event.kind();
+
+                if ( kind == OVERFLOW ) {
+                    continue;
+                }
+
+                WatchEvent< Path > ev = ( WatchEvent< Path > ) event;
+                Path filename = ev.context();
+
+                try {
+                    Path child = dir.resolve( filename );
+                    if ( !Files.probeContentType( child ).equals( "text/plain" ) ) {
+                        System.err.format( "New file '%s' is not a plain text file.%n", filename );
+                        continue;
+                    }
+                } catch ( IOException x ) {
+                    System.err.println( x );
+                    continue;
+                }
+
+                dataReader.processFiles();
+            }
+
+            boolean valid = key.reset();
+            if ( !valid ) {
+                break;
+            }
+        }
+    }
+}
